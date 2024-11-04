882e1013360dc86066c6b0b819a100d486ab1394
mokwongame <ictplusud@gmail.com>
2024-11-04 16:23:12 +0900

    PythonHub 클래스에 Serial method 추가

--- .ipynb_checkpoints/PythonHub-checkpoint.py
+++ .ipynb_checkpoints/PythonHub-checkpoint.py
@@ -10,6 +10,11 @@ class PythonHub:
     # private 멤버 변수
     __defPortName = 'COM3'
     __defPortSpeed = 9600
+    __defWaitTime = 0.1
+
+    # 정적(static) 멤버 함수: 입력에 self가 없음 -> C++에서 static에 해당
+    def wait(waitTime = __defWaitTime):
+        time.sleep(waitTime)
     
     # 생성자(constructor)
     def __init__(self, portName = __defPortName, portSpeed = __defPortSpeed):
@@ -18,5 +23,31 @@ class PythonHub:
     #소멸자(destructor)
     def __del__(self):
         if self.ard.isOpen(): # Serial이 열려있으면(is open?)
-            ard.close() # Serial 닫기(close)
-    
+            self.ard.close() # Serial 닫기(close)
+
+    # Serial Method
+    def writeSerial(self, sCmd):
+        btCmd = sCmd.encode()
+        nWrite = self.ard.write(btCmd)
+        self.ard.flush() # 모든 바이트를 출력으로 내보기기
+        return nWrite
+
+    def readSerial(self):
+        nRead = self.ard.in_waiting
+        if nRead > 0:
+            btRead = self.ard.read(nRead)
+            sRead = btRead.decode()
+            return sRead
+        else: return ''
+
+    def talk(self, sCmd):
+        return self.writeSerial(sCmd + '\n')
+
+    def listen(self):
+        PythonHub.wait() # 정적 멤버 접근할 때는 클래스명을 앞에 써줌
+        sRead = self.readSerial()
+        return sRead.strip() # 문자열 sRead 앞뒤에 공백 제거
+
+    def talkListen(self, sCmd):
+        self.talk(sCmd)
+        return self.listen()
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "PythonHub 클래스의 생성자에 디폴트값 추가"
+git commit -m "PythonHub 클래스에 Serial method 추가"
 git push -u origin master
