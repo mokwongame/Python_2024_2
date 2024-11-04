5bbe2ef64ec166b513831779dbde68748a0cc254
mokwongame <ictplusud@gmail.com>
2024-11-04 16:10:02 +0900

    PythonHub 클래스의 생성자에 디폴트값 추가

--- PythonHub.py
+++ PythonHub.py
@@ -7,9 +7,13 @@ import time
 # __이름 -> private group(private:)
 # self 의미: 현재 인스턴스(instance)의 레퍼런스(reference) -> C++ 기준에서 self = *this
 class PythonHub:
+    # private 멤버 변수
+    __defPortName = 'COM3'
+    __defPortSpeed = 9600
+    
     # 생성자(constructor)
-    def __init__(self):
-        self.ard = Serial('COM3', 9600)
+    def __init__(self, portName = __defPortName, portSpeed = __defPortSpeed):
+        self.ard = Serial(portName, portSpeed)
         
     #소멸자(destructor)
     def __del__(self):
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "PythonHub 클래스의 생성자와 소멸자 작성"
+git commit -m "PythonHub 클래스의 생성자에 디폴트값 추가"
 git push -u origin master
