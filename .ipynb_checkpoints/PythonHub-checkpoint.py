f462cf9be5ef90d01421732dda7cd8cb7a03e9fa
mokwongame <ictplusud@gmail.com>
2024-11-04 15:49:58 +0900

    PythonHub 클래스의 생성자와 소멸자 작성

--- .ipynb_checkpoints/PythonHub-checkpoint.py
+++ .ipynb_checkpoints/PythonHub-checkpoint.py
@@ -1,5 +1,18 @@
 from serial import Serial
 import time
 
+# 파이썬에서 생성자명과 소멸자명은 하나로 정해짐
+# 접근 그룹 규칙
+# __이름__ -> public group(public:)
+# __이름 -> private group(private:)
+# self 의미: 현재 인스턴스(instance)의 레퍼런스(reference) -> C++ 기준에서 self = *this
 class PythonHub:
-    pass
+    # 생성자(constructor)
+    def __init__(self):
+        self.ard = Serial('COM3', 9600)
+        
+    #소멸자(destructor)
+    def __del__(self):
+        if self.ard.isOpen(): # Serial이 열려있으면(is open?)
+            ard.close() # Serial 닫기(close)
+    
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "PythonHub 클래스 작성 시작"
+git commit -m "PythonHub 클래스의 생성자와 소멸자 작성"
 git push -u origin master
